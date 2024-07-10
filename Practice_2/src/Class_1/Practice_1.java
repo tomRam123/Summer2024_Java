83a0375bb08a1eddc7134dc4fa929cde11b69e0e
Tomas <memin170@gmail.com>
2024-07-10 19:43:51 -0400

    Practice 4

--- Practice_2/src/Class_1/Practice_1.java
+++ Practice_2/src/Class_1/Practice_1.java
@@ -2,6 +2,9 @@ package Class_1;
 
 public class Practice_1 {
     public static void main(String[] args) {
-        System.out.println(" I really need to practice ");
+        System.out.println("I really need to practice ");
+        System.out.println("\nI'm still learning");
+
+
     }
 }
